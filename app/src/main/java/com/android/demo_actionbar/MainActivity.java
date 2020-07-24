766135b82190eb1b852da654bdbf1289568089e0
marcusleem <Thengtheng5>
2020-07-24 12:38:43 +0800

    commit

---
+++ app/src/main/java/com/android/demo_actionbar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.android.demo_actionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
